4e672607161eccf4bd07b9fd8b199a5866f8e07b
NaotoKubota <the.owner.of.the.dream123@gmail.com>
2024-05-17 11:11:03 -0700

    Import os

--- src/psi_snakemake.py
+++ src/psi_snakemake.py
@@ -63,10 +63,10 @@ def main():
 
     # Load modules
     import sys
+    import os
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
     sys.path.append(parent_dir)
-    import os
     import time
     import pandas as pd
     from lib import shibalib
--- src/tpm_snakemake.py
+++ src/tpm_snakemake.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 sys.path.append(parent_dir)
