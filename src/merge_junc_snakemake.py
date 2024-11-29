65329b6009722b65960abb568c97ee6bc8372869
NaotoKubota <the.owner.of.the.dream123@gmail.com>
2024-11-28 19:59:02 -0800

    Merge junctions: Add verbose flag and enhance logging in merge_junc_snakemake.py

--- src/merge_junc_snakemake.py
+++ src/merge_junc_snakemake.py
@@ -1,6 +1,10 @@
 import argparse
 import sys
 import pandas as pd
+import logging
+
+# Configure logging
+logger = logging.getLogger(__name__)
 
 def get_args():
 
@@ -12,40 +16,28 @@ def get_args():
 	parser.add_argument("--exonexon", type = str, help = "Exon-exon Junction files", nargs = "+")
 	parser.add_argument("--exonintron", type = str, help = "Exon-intron Junction files", nargs = "+")
 	parser.add_argument("--output", type = str, help = "Output name")
+	parser.add_argument("-v", "--verbose", action = "store_true", help = "Verbose output")
 
 	args = parser.parse_args()
-
 	return(args)
 
-def exonexon():
+def merge_exonexon(filelist):
 
-	print("Merge exon-exon junction count...", file = sys.stdout)
-
-	args = get_args()
-	filelist = args.exonexon
-
+	result_df = pd.DataFrame()
 	for i in range(len(filelist)):
-
-		print(filelist[i].split("/")[-1] + "...", file = sys.stdout)
-
+		logger.debug(filelist[i].split("/")[-1] + "...")
 		junc_df = pd.read_csv(
-
 			filelist[i],
 			sep = "\t",
 			header = None,
 			dtype = str
-
 		)
 
 		sample = filelist[i].split("/")[-1].rstrip("_exon-exon.junc")
-
 		junc_df = junc_df.iloc[:, [0, 1, 2, 4, 10]]
-
 		junc_df.columns = ["chr", "start", "end", "count", "block"]
 		junc_df = junc_df.reset_index()
-
 		junc_df.loc[(junc_df["chr"].str.isdecimal() == True) | (junc_df["chr"].str.len() <= 2), "chr"] = "chr" + junc_df["chr"]
-
 		junc_df["count"] = junc_df["count"].astype("int32")
 		junc_df["blockSize1"] = junc_df["block"].str.split(",", expand = True)[0].astype("int32")
 		junc_df["start"] = junc_df["start"].astype("int32") + junc_df["blockSize1"]
@@ -56,39 +48,21 @@ def exonexon():
 		junc_df = junc_df[["ID", "sample", "count"]]
 		# Group by ID and sample
 		junc_df = junc_df.groupby(["ID", "sample"], as_index = False).sum()
-
-		if "result_df" in locals():
-
-			result_df = pd.concat(
-
-				[result_df, junc_df],
-				axis = 0
-
-			)
-
-		else:
-
-			result_df = junc_df
+		result_df = pd.concat([result_df, junc_df], axis = 0) if result_df is not None else junc_df
 
 	result_df["count"] = result_df["count"].astype("int32")
 	# Check if there are duplicated junctions
 	dup_df = result_df.groupby(["ID", "sample"], as_index = False).count()
 	dup_df = dup_df[dup_df["count"] > 1]
 	if dup_df.shape[0] > 0:
-
-		print("Duplicated junctions:", file = sys.stdout)
-		print(dup_df, file = sys.stdout)
-		print("Please check the input files!", file = sys.stdout)
-		sys.exit(1)
+		logger.error("Duplicated junctions found")
+		raise ValueError("Duplicated junctions detected. Please check the input files.")
 
 	result_df = result_df.pivot(
-
 		index = "ID",
 		columns = "sample",
 		values = "count"
-
 	).fillna(0).reset_index()
-
 	result_df = result_df.rename(columns = {"index": "ID"})
 
 	result_df["chr"] = result_df["ID"].str.split(":", expand = True)[0]
@@ -96,71 +70,41 @@ def exonexon():
 	result_df["end"] = result_df["ID"].str.split(":", expand = True)[1].str.split("-", expand = True)[1].astype("int32")
 	result_df["chr-start"] = result_df["chr"] + "-" + result_df["start"].astype(str)
 	result_df["chr-end"] = result_df["chr"] + "-" + result_df["end"].astype(str)
-
 	col = [i for i in result_df.columns if i not in ["chr", "start", "end", "ID", "chr-start", "chr-end", "mean"]]
 	for j in col:
-
 		result_df = result_df.astype({j: "int32"})
-
 	col = ["chr", "start", "end", "ID"] + col
 	result_df = result_df[col]
 
 	return(result_df)
 
-def exonintron():
+def merge_exonintron(filelist):
 
-	print("Merge exon-intron junction count...", file = sys.stdout)
-
-	args = get_args()
-	filelist = args.exonintron
-
+	result_df = pd.DataFrame()
 	for i in range(len(filelist)):
-
-		print(filelist[i].split("/")[-1] + "...", file = sys.stdout)
-
+		logger.debug(filelist[i].split("/")[-1] + "...")
 		junc_df = pd.read_csv(
-
 			filelist[i],
 			sep = "\t",
 			comment = "#",
 			dtype = str
-
 		)
 
 		sample = filelist[i].split("/")[-1].rstrip("_exon-intron.junc")
-
 		junc_df = junc_df.iloc[:, [1, 2, 3, 0, 6]]
-
 		junc_df.columns = ["chr", "start", "end", "ID", "count"]
 		junc_df["sample"] = sample
-
 		junc_df.loc[(junc_df["chr"].str.isdecimal() == True) | (junc_df["chr"].str.len() <= 2), "chr"] = "chr" + junc_df["chr"]
-
-		if "result_df" in locals():
-
-			result_df = pd.concat(
-
-				[result_df, junc_df],
-				axis = 0
-
-			)
-
-		else:
-
-			result_df = junc_df
+		result_df = pd.concat([result_df, junc_df], axis = 0) if result_df is not None else junc_df
 
 	result_df["count"] = result_df["count"].astype("int32")
 	result_df = result_df.pivot(
-
 		index = ["chr", "start", "end", "ID"],
 		columns = "sample",
 		values = "count"
-
 	).fillna(0).reset_index()
-
 	col = [i for i in result_df.columns if i not in ["chr", "start", "end", "ID"]]
 	for j in col:
-
 		result_df = result_df.astype({j: "int32"})
 
 	return(result_df)
@@ -168,32 +112,42 @@ def exonintron():
 def main():
 
 	args = get_args()
+	# Set up logging
+	logging.basicConfig(
+		format="[%(asctime)s] %(levelname)7s %(message)s",
+		level=logging.DEBUG if args.verbose else logging.INFO,
+	)
+	logger.info("Starting merge junctions")
+	logger.debug(args)
+
+	# Parse args
+	exonexon = args.exonexon
+	exonintron = args.exonintron
+	output = args.output
 
 	# exon-exon junctions
-	exon_exon_junc_df = exonexon()
+	logger.info("Merge exon-exon junction count...")
+	exon_exon_junc_df = merge_exonexon(exonexon)
 
 	# exon-intron junctions
-	exon_intron_junc_df = exonintron()
+	logger.info("Merge exon-intron junction count...")
+	exon_intron_junc_df = merge_exonintron(exonintron)
 
+	# Combine and save results
+	logger.info("Combine and save results...")
 	result_df = pd.concat(
-
 		[exon_exon_junc_df, exon_intron_junc_df]
-
-	)
-
-	result_df = result_df.sort_values(["chr", "start"])
-
+	).sort_values(["chr", "start"])
 	result_df.to_csv(
-
 		args.output,
 		sep = "\t",
 		index = False
-
 	)
 
 	junc_num = str(result_df.count()[0])
-	print("Total number of junctions: " + junc_num, file = sys.stdout)
+	logger.debug(f"Total number of junctions: {junc_num}")
+	logger.info("Merge junctions completed")
 
 if __name__ == '__main__':
 
-    main()
+	main()
