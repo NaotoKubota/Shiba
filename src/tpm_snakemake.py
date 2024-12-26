080d319961eb0f8c7fd689d864f2516d6cfb2650
NaotoKubota <the.owner.of.the.dream123@gmail.com>
2024-12-25 19:34:29 -0800

    Add logging to tpm_snakemake.py for better traceability and verbosity control

--- src/tpm_snakemake.py
+++ src/tpm_snakemake.py
@@ -7,6 +7,10 @@ sys.path.append(parent_dir)
 from lib import expression
 import pandas as pd
 from styleframe import StyleFrame, Styler, utils
+import logging
+
+# Configure logging
+logger = logging.getLogger(__name__)
 
 def str2bool(v):
     if v == "True":
@@ -23,25 +27,20 @@ def get_args():
 		description = "Calculate TPM and CPM"
 	)
 
-	parser.add_argument("--countfiles", type = str, help = "Text file of all exon-exon junction files to be processed", nargs = "+")
+	parser.add_argument("--countfiles", type = str, help = "Text file of read counts to be processed", nargs = "+")
 	parser.add_argument("--onlypsi", type = str2bool, help = "Onlypsi", nargs = "?", const = True, default = False)
 	parser.add_argument("--onlypsi-group", type = str2bool, help = "Onlypsi-group", nargs = "?", const = True, default = False)
 	parser.add_argument("--output", type = str, help = "Output directory")
-
+	parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
 	args = parser.parse_args()
-
 	return(args)
 
 def merge_table(countfiles):
 
 	count_all_df = pd.DataFrame()
-
 	for i in range(len(countfiles)):
-
 		sample = countfiles[i].split('/')[-1].rstrip("_counts.txt")
-
 		count_df = pd.read_csv(
-
 			countfiles[i],
 			sep = "\t",
 			skiprows = 1,
@@ -51,23 +50,16 @@ def merge_table(countfiles):
 				5: "int32",
 				6: "int32"
 			}
-
 		)
 
 		count_df.columns = count_df.columns[0:2].tolist() + [sample]
-
 		if count_all_df.empty:
-
 			count_all_df = count_df
-
 		else:
-
 			count_all_df = pd.merge(
-
 				count_all_df,
 				count_df[["Geneid", sample]],
 				on = "Geneid"
-
 			)
 
 	count_all_df = count_all_df.rename(columns = {"Geneid": "gene_name"})
@@ -75,79 +67,55 @@ def merge_table(countfiles):
 	return(count_all_df)
 
 def main():
-
+	## Main
 	args = get_args()
+	# Set up logging
+	logging.basicConfig(
+		format = "[%(asctime)s] %(levelname)7s %(message)s",
+		level = logging.DEBUG if args.verbose else logging.INFO
+	)
+	logger.info("Starting TPM and CPM calculation")
+	logger.debug(args)
 
-	print("Merge count tables...", file = sys.stderr)
+	# Merge count tables
+	logger.info("Merge count tables...")
 	merged_table_df = merge_table(args.countfiles)
 	count_df = merged_table_df.drop(columns = ["Length"])
 
 	if args.onlypsi == False and args.onlypsi_group == False:
-
 		count_df.to_csv(
-
-			args.output + "counts.txt",
+			os.path.join(args.output, "counts.txt"),
 			sep = "\t",
 			index = False
-
 		)
 
-	# copy count table
-	count_a_df = merged_table_df.copy()
-	count_b_df = merged_table_df.copy()
-	del merged_table_df
-
-	print("Calculate TPM...", file = sys.stderr)
-	expression_processor = expression.ExpressionProcessor(count_a_df)
-	tpm_df = expression_processor.TPM()
-	del count_a_df
-
-	tpm_df.to_csv(
-
-		args.output + "TPM.txt",
-		sep = "\t",
-		index = False
-
-	)
-
-	print("Calculate CPM...", file = sys.stderr)
-	expression_processor = expression.ExpressionProcessor(count_b_df)
-	cpm_df = expression_processor.CPM()
-	del count_b_df
-
-	cpm_df.to_csv(
-
-		args.output + "CPM.txt",
-		sep = "\t",
-		index = False
-
-	)
+	# Calculate TPM and CPM
+	logger.info("Calculate TPM...")
+	tpm_df = expression.ExpressionProcessor(merged_table_df.copy()).TPM()
+	tpm_df.to_csv(os.path.join(args.output, "TPM.txt"), sep = "\t", index = False)
+	logger.info("Calculate CPM...")
+	cpm_df = expression.ExpressionProcessor(merged_table_df.copy()).CPM()
+	cpm_df.to_csv(os.path.join(args.output, "CPM.txt"), sep = "\t", index = False)
 
 	# Excel file
-	print("Export to an excel file....", file = sys.stderr)
-
-	# Style
+	logger.info("Exporting results to Excel...")
+	# StyleFrame
 	style = Styler(
-
 		horizontal_alignment = utils.horizontal_alignments.left,
 		border_type = utils.borders.default_grid,
 		wrap_text = False
-
 	)
-
-	with StyleFrame.ExcelWriter(args.output + "TPM_CPM.xlsx") as writer:
-
+	with StyleFrame.ExcelWriter(os.path.join(args.output, "TPM_CPM.xlsx")) as writer:
 		tpm_sf = StyleFrame(tpm_df)
 		tpm_sf.set_column_width(columns = tpm_df.columns, width = 20)
 		tpm_sf.apply_column_style(cols_to_style = tpm_df.columns, styler_obj = style, style_header = True)
 		tpm_sf.to_excel(writer, index = False, columns_and_rows_to_freeze = "B2", sheet_name = "TPM")
-
 		cpm_sf = StyleFrame(cpm_df)
 		cpm_sf.set_column_width(columns = cpm_df.columns, width = 20)
 		cpm_sf.apply_column_style(cols_to_style = cpm_df.columns, styler_obj = style, style_header = True)
 		cpm_sf.to_excel(writer, index = False, columns_and_rows_to_freeze = "B2", sheet_name = "CPM")
 
+	logger.info("TPM and CPM calculation completed")
+
 if __name__ == '__main__':
-
-    main()
-
+	main()
